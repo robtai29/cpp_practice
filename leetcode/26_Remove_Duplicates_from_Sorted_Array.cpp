74614093fb61a31d881132be84396066485a570d
Robert Tai <robtai29@gmail.com>
2021-07-23 13:08:52 -0700

    7.23.2021

---
+++ leetcode/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+7.23.2021
+*******************************************************************************/
+
+class Solution {
+public:
+  int removeDuplicates(vector<int>& nums) {
+    if (nums.size() == 0) return 0;
+    
+    int slow = 0;
+    int fast = 0;
+    int size = nums.size();
+      
+     while (fast < size){
+         
+         if (nums[slow] == nums[fast]){
+             fast++;
+             
+         }else{
+             ++slow;
+            nums[slow] = nums[fast];
+            fast++;
+     }
+     }
+      nums.erase(begin(nums) + slow + 1, nums.end());
+      return nums.size();
+  }
+};
+
