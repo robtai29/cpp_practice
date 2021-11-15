17842a7930aa3dfb6baa4386909fd8481c35b77f
Robert Tai <robtai29@gmail.com>
2021-11-14 22:07:37 -0800

    11.14.21

--- leetcode/103_Binary Tree_Zigzag_Level_Order_Traversal.cpp
+++ leetcode/103_Binary Tree_Zigzag_Level_Order_Traversal.cpp
@@ -15,8 +15,47 @@ https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+void zigzagTravel(TreeNode* node, vector<vector<int>>& result, int level){
+    if (node == nullptr){
+        return;
+    }
+    
+    if (result.size() == level){
+        result.push_back(vector<int>{});
+    }
+    
+    if (level % 2 == 0){
+        result[level].push_back(node -> val);
+    }else{
+        result[level].insert(result[level].begin(), node-> val);
+    }
+    
+    zigzagTravel(node->left, result, level + 1);
+    zigzagTravel(node->right, result, level + 1);
+}
+    
 public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        zigzagTravel(root, res, 0);
+        return res;
+    }
+};
+
+/*
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> res;
         if (root == nullptr){
@@ -50,4 +89,4 @@ public:
         }
         return res;
     }
-};
+*/
--- leetcode/104_Maximum_Depth_of_Binary_Tree.cpp
+++ leetcode/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
 https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
-7.8.21
+11.14.21
 *******************************************************************************/
 
 /**
@@ -15,18 +15,54 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    //bfs
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr){
+            return 0;
+        }  
+        
+        queue<TreeNode*> q;
+        int level = 0;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if (node -> left){
+                    q.push(node-> left);
+                }
+                if (node -> right){
+                    q.push(node-> right);
+                }
+            }
+            level++;
+        }
+            return level;
+    }
+};
+
+/*
     int maxDepth(TreeNode* root) {
        if (root == nullptr){
-            return 0;
-        }
+           return 0;
+       }
         
-        int left = maxDepth(root-> left);
+        int left = maxDepth(root -> left);
         int right = maxDepth(root -> right);
-        
-        return max(left, right) + 1;
-        
+        return 1 + max(left, right);
     }
-
-};
+*/
