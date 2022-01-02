4afbf86a82a2fc2a1fdb59186e3c0579c43bf61d
Robert Tai <robtai29@gmail.com>
2022-01-01 20:48:25 -0800

    1.1.22

--- leetcode/198_House_Robber.cpp
+++ leetcode/198_House_Robber.cpp
@@ -1,13 +1,13 @@
 /******************************************************************************
 https://leetcode.com/problems/house-robber/
 
-9.9.2021
+9.9.2021, 1/1/22
 *******************************************************************************/
 
 class Solution {
 public:
     //recursive relationship: rob(i) = max(rob(i-2) + currenthouse, rob(i-1))
-    
+    //top down
     int rob(vector<int>& nums, vector<int>& m, int i){
         if (i < 0){
             return 0;
@@ -30,20 +30,21 @@ public:
 /*
 class Solution {
 public:
+    //bottom up
     int rob(vector<int>& nums) {
+        //each cell represents the maximum money from beginning to ith element, or local max thus far
         vector<int> memo(nums.size(), 0);
         
         memo[0] = nums[0];
-        if (memo.size() > 1){
+        if (nums.size() > 1){
             memo[1] = max(nums[0], nums[1]);
         }
         
-        for (int i = 2; i < memo.size(); i++){
-
-            memo[i] = max(memo[i-2] + nums[i], memo[i-1]);
+        for (int i = 2; i < nums.size(); i++){
+            memo[i] = max(memo[i-1], memo[i-2] + nums[i]);   
         }
         
-        return memo[memo.size()-1];
+        return memo[nums.size()-1];
     }
 };
 */
