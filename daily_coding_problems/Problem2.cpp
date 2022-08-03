158156ccc805bd5ba2f25194cec98c2f6951ffb9
Rob Tai <61960571+robtai29@users.noreply.github.com>
2022-08-03 08:08:45 -0700

    Delete daily_coding_problems directory

--- daily_coding_problems/Problem2.cpp
+++
@@ -1,7 +0,0 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
